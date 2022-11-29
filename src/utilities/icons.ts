1d01e96690267f5709882117afb31332f992c521
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-29 19:14:29 +0100

    ✨ impport all icons from react icon

---
+++ src/utilities/icons.ts
@@ -0,0 +1,28 @@
+import {
+  HiOutlineHandThumbUp as LikeIcon,
+  HiPlus as PlusIcon,
+  HiOutlineChevronDown as ShowMoreIcon,
+  HiOutlineSpeakerWave as SoundIcon,
+  HiOutlineSpeakerXMark as MutedIcon,
+} from 'react-icons/hi2'
+
+import {
+  AiOutlineSearch as SearchIcon,
+  AiFillCaretDown as ArrowDownIcon,
+} from 'react-icons/ai'
+
+import { IoPlaySharp as PlayIcon } from 'react-icons/io5'
+
+import { BsDot as DotIcon } from 'react-icons/bs'
+
+export {
+  LikeIcon,
+  PlusIcon,
+  ShowMoreIcon,
+  SoundIcon,
+  MutedIcon,
+  SearchIcon,
+  ArrowDownIcon,
+  PlayIcon,
+  DotIcon,
+}
