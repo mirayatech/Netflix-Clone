471236f5d0989a3f0d3901be42732adfa7ecae95
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-11-30 05:40:32 +0100

    🎨 color variables

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  White: '#fff',
+  Black: '#000',
+  Body: ' #141414',
+  Card: '#181818',
+  GreyText: ' #808080',
+  TransparentGrey: ' #7777',
+  TransparentBlack: '#000000bf',
+  Red: ' #e50a13',
+  Green: ' #38a353',
+}
