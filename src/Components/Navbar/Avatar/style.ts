be05bdafe0b8beef20cc3638444e2f8fe702e13a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-01 19:47:53 +0100

    🎨 style avatar

---
+++ src/Components/Navbar/Avatar/style.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import { flexCenter, theme } from '../../../styles'
+
+export const Wrapper = styled.div`
+  ${flexCenter}
+  position: relative;
+`
+
+export const Button = styled.button`
+  border: none;
+  padding: 5px;
+  ${flexCenter}
+  background-color: transparent;
+  transition: all 0.3s ease;
+`
+
+export const Image = styled.img`
+  width: 35px;
+  border-radius: 3px;
+`
+
+export const Arrow = styled.span`
+  margin-left: 10px;
+  color: ${theme.White};
+`
+
+export const Menu = styled.div`
+  top: 45px;
+  right: 30px;
+  ${flexCenter}
+  width: 100px;
+  padding: 15px 0;
+  border-radius: 4px;
+  position: absolute;
+  background-color: ${theme.TransparentBlack};
+  transition: all 0.3s ease;
+`
+
+export const SignOut = styled.button`
+  border: none;
+  font-weight: 600;
+  color: ${theme.White};
+  background-color: transparent;
+
+  :hover {
+    text-decoration: underline;
+  }
+`
