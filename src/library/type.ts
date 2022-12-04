6c08cc7d828215a17157c5c1ac9dd6bc511ba713
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-04 12:25:32 +0100

    🗂 movie types

---
+++ src/library/type.ts
@@ -0,0 +1,10 @@
+export type MovieType = {
+  poster_path: string
+  backdrop_path: string
+  genre_ids: []
+  id: number
+  original_name: string
+  overview: string
+  vote_average: number
+  release_date: string
+}
