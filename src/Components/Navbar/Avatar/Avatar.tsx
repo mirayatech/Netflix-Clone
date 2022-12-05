428bcce5c38942884699dfbf4607da07077f0bc9
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-05 13:27:23 +0100

    🎉 able to sign out

--- src/Components/Navbar/Avatar/Avatar.tsx
+++ src/Components/Navbar/Avatar/Avatar.tsx
@@ -2,9 +2,39 @@ import './avatar-style.css'
 import { useState } from 'react'
 import { ArrowDownIcon } from '../../../utilities'
 import { Button, Wrapper, Image, Arrow, SignOut, Menu } from './style'
+import { signOut } from 'firebase/auth'
+import { firebaseAuth } from '../../../library'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { theme } from '../../../styles'
 
 export function Avatar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const navigate = useNavigate()
+  const onSignOut = async () => {
+    try {
+      await signOut(firebaseAuth)
+
+      toast('Successfully signed out.', {
+        icon: '✅',
+        style: {
+          borderRadius: '10px',
+          background: theme.Body,
+          color: theme.White,
+        },
+      })
+      navigate('/browse')
+    } catch (error) {
+      toast('Something went wrong.', {
+        icon: '❌',
+        style: {
+          borderRadius: '10px',
+          background: theme.Body,
+          color: theme.White,
+        },
+      })
+    }
+  }
   return (
     <Wrapper>
       <Button
@@ -23,7 +53,7 @@ export function Avatar() {
           onMouseEnter={() => setIsMenuOpen(true)}
           onMouseLeave={() => setIsMenuOpen(false)}
         >
-          <SignOut>Sign Out</SignOut>
+          <SignOut onClick={onSignOut}>Sign Out</SignOut>
         </Menu>
       )}
     </Wrapper>
