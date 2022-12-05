b3c00022dd1cb3a7caabb902d8f882e3aeef0b56
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-12-05 11:02:17 +0100

    🎉 able to sign in

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,26 +1,67 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { Button, Container, Form, Info, Input, Title } from './style'
+import toast from 'react-hot-toast'
+import { firebaseAuth } from '../../library'
+import { useState } from 'react'
 
 export function SignIn() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSignUp = async (event: any) => {
+    event.preventDefault()
+
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, email, password)
+      toast('Successfully logged in.', {
+        icon: '✅',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      })
+      navigate('/')
+    } catch (error) {
+      toast('Something went wrong, either email or password is incorrect.', {
+        icon: '❌',
+        style: {
+          width: '300px',
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      })
+    }
+  }
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={onSignUp}>
         <Title>Sign in</Title>
 
         <label className="sr-only" htmlFor="Email">
           Email
         </label>
-        <Input type="email" id="Email" placeholder="Email" />
+        <Input
+          type="email"
+          id="Email"
+          placeholder="Email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
 
         <label className="sr-only" htmlFor="Password">
           Password
         </label>
-        <Input type="password" placeholder="Password" />
+        <Input
+          type="password"
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-        <Link to="/browse">
-          {' '}
-          <Button type="submit">Sign In</Button>
-        </Link>
+        <Button type="submit">Sign In</Button>
 
         <Info>
           New to Netflix?
